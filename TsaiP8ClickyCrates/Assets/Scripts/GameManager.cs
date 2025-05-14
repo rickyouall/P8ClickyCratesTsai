0e27eef0d6e841cccf7f7f14876ba9cb8c77b52b
rickyouall <740502@student.dusd.net>
2025-05-14 15:27:33 -0700

    making the prefabs
    Today i made prefabs and game manager.

---
+++ TsaiP8ClickyCrates/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public GameObject[] targets2;
+
+    private float spawnRate = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTargets());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTargets()
+    {
+        while (true)
+        {
+
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+}
